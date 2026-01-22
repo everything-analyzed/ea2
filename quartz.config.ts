75106ad38fe3284fcdc0ba41a80cefef7e03c76a
everything-analyzed <tiffanycfoggie@gmail.com>
2026-01-22 01:52:30 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://everything-analyzed.github.io/ea2",
+    baseUrl: "everything-analyzed.github.io/ea2",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
